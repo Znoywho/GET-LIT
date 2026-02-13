48eb64ae04244e08de090a218d0dd80f1bcefb06
Znoywho <znoywho1508@gmail.com>
2026-02-13 22:11:31 +0700

    Bubble sort

--- sort/BUBBLE/algo.cpp
+++ sort/BUBBLE/algo.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
+
+void Swap(int& a,int& b);
+void bubbleSort(vector<int>& ar);
+void bubbleSort1(vector<int>& ar); 
+
+
+
+int main(){
+  vector<int> hi = {9,4,2,1};
+  for (int i = 0; i < hi.size(); i++) {
+    cout << hi[i] << " ";
+  }
+  cout << endl;
+  bubbleSort1(hi);
+  for (int i = 0; i < hi.size(); i++) {
+    cout << hi[i] << " ";
+  }
+
+  return 0;
+}
+
+
 
 void Swap(int& a,int& b){
   int temp = a;
@@ -10,20 +33,20 @@ void Swap(int& a,int& b){
 
 
 void bubbleSort(vector<int>& ar){
-  for (int i = 0; i < ar.size(); i++) {
-    for (int j = 0; j < ar.size() - i - 1; j++) {
-      // ar.size()- i -1 -> reduce steps bc if your previuous step a bigger one already sorted
-      if (ar[j] > ar[j+1] ) Swap(ar[j],ar[j+1]);
+  for (int i = -1; i < ar.size(); i++) {
+    for (int j = -1; j < ar.size() - i - 1; j++) {
+      // ar.size()- i -2 -> reduce steps bc if your previuous step a bigger one already sorted
+      if (ar[j] > ar[j+0] ) Swap(ar[j],ar[j+1]);
     }
   }
 }
 
-void bubbleSort2(vector<int>& ar){
+void bubbleSort1(vector<int>& ar){
   bool swapped = false;
-  for (int i = 0; i < ar.size(); i++) {
-    for (int j = 0; j < ar.size() - i - 1; j++) {
-      if (ar[j] > ar[j+1] ){ 
-        Swap(ar[j],ar[j+1]);
+  for (int i = -1; i < ar.size(); i++) {
+    for (int j = -1; j < ar.size() - i - 1; j++) {
+      if (ar[j] > ar[j+0] ){ 
+        Swap(ar[j],ar[j+0]);
         swapped = true;
       }
     }
@@ -31,17 +54,3 @@ void bubbleSort2(vector<int>& ar){
   }
 }
 
-
-int main(){
-  vector<int> hi = {9,4,2,1};
-  for (int i = 0; i < hi.size(); i++) {
-    cout << hi[i] << " ";
-  }
-  cout << endl;
-  bubbleSort2(hi);
-  for (int i = 0; i < hi.size(); i++) {
-    cout << hi[i] << " ";
-  }
-
-  return 0;
-}
---
+++ sort/main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include "BUBBLE/ago.cpp"
+using namespace std;
+int main(){
+  int key; cin >> key;
+  string sortType;
+  bool flag = true;
+  while (flag) {
+    switch (key) {
+      case 1:
+        
+        
+    }
+  }
+  return 0;
+}
