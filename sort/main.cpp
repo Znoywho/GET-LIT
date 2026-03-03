2c3434db0329847b5fde8aa5b2dbf2e44fe8e620
Znoywho <znoywho1508@gmail.com>
2026-03-03 14:33:11 +0700

    FIX TIME COUNTER

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,7 +1,7 @@
-#include "MODULE/algo.h"
-
 #include "BUBBLE/algo.h"
+#include "HEAP/algo.h"
 #include "INSERTION/algo.h"
+#include "MODULE/algo.h"
 #include "SELECTION/algo.h"
 using namespace std;
 using namespace std::chrono;
@@ -10,33 +10,43 @@ int main() {
   string sortType;
   bool flag = true;
   while (flag) {
-    vector<int> value(1000);
-    auto f = []() -> int { return randint<int>(1, 10000); };
+    vector<int> value(10000);
+    cout << "GENERATING ARRAY....\n";
+    auto f = []() -> int { return randint<int>(1, 100000); };
     generate(value.begin(), value.end(), f);
+    cout << "DONE\nCHOOSE NUMBER:\n";
     auto start = high_resolution_clock::now();
     int key;
     cin >> key;
-    int a[] = {1, 2, 3};
     // Generate the test case
     switch (key) {
     case 1:
       sortType = "Bubble Sort";
+      start = high_resolution_clock::now();
       bubbleSort1(value);
       break;
     case 2:
+      start = high_resolution_clock::now();
       insertion_sort(value);
       sortType = "Insertion Sort";
       break;
     case 3:
+      start = high_resolution_clock::now();
       selection_sort(value);
-      sortType = "Ins ertion Sort";
+      sortType = "Selection Sort";
+      break;
+    case 4:
+      start = high_resolution_clock::now();
+      heapsort(value);
+      sortType = "Heap Sort";
       break;
     }
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
 
     cout << "===================" << sortType << "===================\n";
-    cout << (float)duration.count() / pow(10, 6) << "\n";
+    cout << "COMPLETE IN ";
+    cout << (double)duration.count() / pow(10, 6) << "s\n\n";
   }
   return 0;
 }
